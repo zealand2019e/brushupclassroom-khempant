5b7ff0d118c94a88559db6852910c3a35bcea03d
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-02-11 11:20:03 +0100

    Opgave 3

--- ClassRoom/Program.cs
+++ ClassRoom/Program.cs
@@ -10,6 +10,16 @@ namespace ClassRoom
     {
         static void Main(string[] args)
         {
+            //opgave 3
+            var klasserum = new KlasseRum();
+            klasserum.KlasseNavn = "Zealand-3";
+            klasserum.SemesterStart = new DateTime(2019, 1, 1);
+            klasserum.Klasseliste.Add(new studerende("Hans", 5, 7));
+            klasserum.Klasseliste.Add(new studerende("Grete", 3, 20));
+            klasserum.Klasseliste.Add(new studerende("Bo", 10, 1));
+
+
+
         }
     }
 }
