79c8ad7bced30fb41e7f749a68e5982846ab92e6
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-02-11 09:59:22 +0100

    Opgave 2 Tilføjet klassen KlasseRum

---
+++ ClassRoom/KlasseRum.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class KlasseRum
+    {
+        public string KlasseNavn { get; set; }
+
+        public List<studerende> Klasseliste { get; set; }
+
+        public DateTime SemesterStart { get; set; }
+
+        public KlasseRum()
+        {
+            Klasseliste = new List<studerende>();
+            SemesterStart = new DateTime();
+        }
+    }
+}
