b01e60c85585fe0da4d78896c274b145083cd756
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-02-11 09:55:57 +0100

    Opgave 1 Tilføjet studerende klasse

---
+++ ClassRoom/studerende.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class studerende
+    {
+        public string Navn { get; set; }
+
+        public int FødselsMåned { get; set; }
+
+        public int FødselsDag { get; set; }
+
+        public studerende(string navn, int måned, int dag)
+        {
+            Navn = navn;
+            FødselsMåned = måned;
+            FødselsDag = dag;
+
+        }
+
+    }
+}
