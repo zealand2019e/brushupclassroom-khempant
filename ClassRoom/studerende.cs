bebc3fe3a20e1cc880f565bce565cc75b97babf6
Martin Kierkegaard <martinkierkegaard@yahoo.com>
2019-02-11 11:52:59 +0100

    Opgave 5 switch statement som viser hvilken årstid en studerende er født på

--- ClassRoom/studerende.cs
+++ ClassRoom/studerende.cs
@@ -21,9 +21,36 @@ namespace ClassRoom
             FødselsDag = dag;
         }
 
+        public string Årstid()
+        {
+            switch (FødselsMåned)
+            {
+                case 12:
+                case 1:
+                case 2:
+                    return "Vinter";
+                case 3:
+                case 4:
+                case 5:
+                    return "Forår";
+                case 6:
+                case 7:
+                case 8:
+                    return "Sommer";
+                case 9:
+                case 10:
+                case 11:
+                    return "Efterår";
+                default:
+                    return $"Denne måned: {FødselsMåned} findes ikke skal være i 1-12";
+
+            }
+
+        }
+
         public override string ToString()
         {
-            return $"Navn: {Navn} , måned: {FødselsMåned}, dag: {FødselsDag}";
+            return $"Navn: {Navn} , måned: {FødselsMåned}, dag: {FødselsDag}, Årstid: {Årstid()}";
         }
 
 
