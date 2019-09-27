466d76cd466082303c0e8e0fd9d25064be688764
Khem Pant <khem0026@edu.easj.dk>
2019-09-28 00:06:09 +0200

    RevisionStep1

---
+++ ClassRoom/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoom
+{
+    public class Student
+    {
+        private string name;
+        private int birth_day;
+        private int birth_month;
+
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        public int BirthDay
+        {
+            get { return birth_day; }
+            set { birth_day = value; }
+        }
+        public int BirthMonth
+        {
+            get { return birth_month; }
+            set { birth_month = value; }
+        }
+        public Student(int bday, int bmonth, string name)
+        {
+            Name = name;
+            BirthDay = bday;
+            BirthMonth = bmonth;
+        }
+        
+    }
+}
